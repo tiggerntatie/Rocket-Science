65352a7cc949e00e2e1acc4192fc0e9870cc073b
Eric Dennison <ericd@netdenizen.com>
2018-03-04 13:20:03 -0500

    Updated from Brython Server: 3/4/2018 1:20:02 PM

--- rocket2.py
+++ rocket2.py
@@ -2,6 +2,8 @@ from ggrocket import Rocket, Planet
 from math import radians, sqrt
 from ggmath import Slider
 
+earth = Planet()
+
 # Constants relating to Earth and physics
 Re = 6.371E6  # Earth radius: 6371000 meters in scientific notation
 Me = 5.972E24 # Earth mass in kg (5.972 x 10^24)
@@ -14,4 +16,5 @@ print("Predicted escape velocity is ", Ve, " m/s")
 # Add a slider for conrolling the timezoom
 tz = Slider((10,400), 0, 5, 0, positioning="physical")
 
-Planet(Rocket, heading=radians(90), directiond=90, velocity=Ve, timezoom=tz)
+rocket = Rocket(earth, heading=radians(90), directiond=90, velocity=Ve, timezoom=tz)
+earth.run(rocket)
