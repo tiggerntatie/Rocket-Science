894a833fc0cbb00c191dfcb70e2ec401ac7679ff
Eric Dennison <ericd@netdenizen.com>
2018-03-04 11:29:48 -0500

    Updated from Brython Server: 3/4/2018 11:29:48 AM

--- rocket2.py
+++ rocket2.py
@@ -1,4 +1,12 @@
 from ggrocket import Rocket, Planet
-from math import radians
+from math import radians, sqrt
 
-Planet(Rocket, heading=radians(90), directiond=90, velocity=10)
+# Constants relating to Earth and physics
+Re = 6.371E6  # Earth radius: 6371000 meters in scientific notation
+Me = 5.972E24 # Earth mass in kg (5.972 x 10^24)
+G = 6.674E-11 # Gravitational constant
+
+# Calculate the escape velocity from Earth's surface radius
+Ve=sqrt(2*Me*G/Re)
+
+Planet(Rocket, heading=radians(90), directiond=90, velocity=Ve)
