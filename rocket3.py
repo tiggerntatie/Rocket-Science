6904e159d115490c6b1e73fd670fb6350921e0d4
Eric Dennison <ericd@netdenizen.com>
2018-03-06 10:56:02 -0500

    Updated from Brython Server: 3/6/2018 10:56:00 AM

--- rocket3.py
+++ rocket3.py
@@ -8,12 +8,12 @@ RocketStarted = False
 timer = Timer()
 
 # Falcon F9R specifications
-me = 25600      # Empty mass
-mp =  395700    # Propellent mass
-F1D = 716000    # Single engine thrust
-N1D = 9         # Number of rocket engines
-Ftotal = F1D * N1D
-tburn = 180
+me = 25600          # Empty mass
+mp =  395700        # Propellent mass
+F1D = 716000        # Single engine thrust (Newtons)
+N1D = 9             # Number of rocket engines
+Ftotal = F1D * N1D  # Total thrust (Newtons)
+tburn = 180         # Burn time (seconds)
 
 # Predict the final velocity
 vmax = N1D*F1D*tburn/(me+mp)
@@ -26,20 +26,22 @@ def GetThrust():
     else:
         return 0
 
-# Function for starting the rocket thrust
+# Function for starting the rocket thrust (called by the START "button")
 def StartRocket():
-    global RocketStarted
     if not RocketStarted:
         RocketStarted = True
+        # Start a timer that will STOP the rocket engine
         timer.callAfter(tburn, StopRocket)
     
 # Function for stopping the rocket thrust (called by timer)
 def StopRocket(timer):
-    global RocketStarted
-    RocketStarted = False
+    if RocketStarted:
+        RocketStarted = False
     
 # Create a button for starting the simulation
+# Physical positioning at 10,400 pixels, calls the StartRocket function
 start = InputButton((10,400), "START", StartRocket, positioning="physical", size=15)
 
+#Create and "run" the rocket
 rocket = Rocket(earth, thrust=GetThrust, mass=me+mp)
 earth.run(rocket)
