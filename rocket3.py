e86a5c0533e5aa3d5a423107c4491867431eb20b
Eric Dennison <ericd@netdenizen.com>
2018-03-08 15:18:50 -0500

    Updated from Brython Server: 3/8/2018 3:18:49 PM

--- rocket3.py
+++ rocket3.py
@@ -6,9 +6,8 @@ from ggmath import InputButton, Timer
 earth = Planet()  # no gravity to simplify things
 
 RocketStarted = False
-timer = Timer()
-starttime = None    # to keep track of when burn started
-burntime = 0        # to keep track of how long the burn has lasted
+StartTime = None    # to keep track of when burn started
+BurnTime = 0        # keep track of how much time the burn has lasted
 
 # Falcon F9R specifications
 me = 25600          # Empty mass
@@ -28,8 +27,18 @@ print("Predicted final velocity (Rocket Equation), vmax: ", vmaxre, " m/s")
 
 # Create a function for determining the rocket thrust
 def GetThrust():
+    global BurnTime
     if RocketStarted:
-        return Ftotal
+        # get the burn time: seconds since start
+        BurnTime = rocket.shiptime - StartTime
+        # is it time to stop the rocket?
+        if BurnTime >= tburn:
+            # stop the rocket and report zero thrust
+            RocketStarted = False
+            return 0
+        else:
+            # still burning, report full thrust
+            return Ftotal
     else:
         return 0
 
@@ -39,26 +48,16 @@ def StartRocket():
     global starttime
     if not RocketStarted:
         RocketStarted = True
-        # Start a timer that will STOP the rocket engine
-        timer.callAfter(tburn, StopRocket)
         # Note the starting time
-        starttime = timer.time
+        StartTime = rocket.shiptime
     
-# Function for stopping the rocket thrust (called by timer)
-def StopRocket(timer):
-    global RocketStarted
-    if RocketStarted:
-        RocketStarted = False
-        
 # Function for calculating the total rocket mass, based on burn time and total
 # propellent mass.
 def GetMass():
     global RocketStarted
     if RocketStarted:
-        # update the burn time: seconds since start
-        burntime = timer.time - starttime
         # calculate empty mass plus a fraction of the propellent mass based on time
-        return me + mp*(tburn-burntime)/tburn
+        return me + mp*(tburn-BurnTime)/tburn
     else:
         # not started: just return the full pre-launch rocket mass
         return me + mp
