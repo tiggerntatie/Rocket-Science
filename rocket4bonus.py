972146d8225c0c98a7d2e7f62d6de6071441cf2f
Eric Dennison <ericd@netdenizen.com>
2018-03-12 15:22:46 -0400

    Updated from Brython Server: 3/12/2018 3:22:46 PM

--- rocket4bonus.py
+++ rocket4bonus.py
@@ -1,425 +1,67 @@
-# ggrocket - ggmath extensions for modeling spacecraft in planetary orbit
+# Lundar Lander Simulation
+from math import radians, sqrt, log
+from ggmath import Label, Slider
+from ggame import Color
+from ggrocket import Rocket, Planet
 
-from math import pi, degrees, radians, atan2, sin, cos, sqrt
-from ggame import LineStyle, Color
-from ggmath import MathApp, Circle, ImagePoint, Timer, Label
-from ggrocket import Planet, Rocket
+G = 6.673E-11   # universal gravitation constant
+moonmass = 7.3477E22
+moonradius = 1738000
+alt = 15000     # approximately 15 km orbital altitude
+vel = sqrt(G*moonmass/(moonradius + alt))
 
-class Rocket(ImagePoint):
+# Lunar Lander specifications
+mascent = 4700          # Ascent stage gross (fueled) mass
+mdescent = 10344        # Descent stage gross (fueled) mass
+mdescfuel = 8200        # Descent stage fuel mass
+Fdmax = 45040           # Descent stage maximum thrust
+SpecIdesc = 3050        # Descent specific impulse (Ns/kg)
+mdotmax = Fdmax/SpecIdesc # Maximum propellent flow rate
+MinThrottle = 0.1       # 10% minimum throttle
+MaxThrottle = 0.6       # 60% full throttle
+MinThrust = MinThrottle * Fdmax
+MaxThrust = MaxThrottle * Fdmax
 
+
+class Lem(Rocket):
     def __init__(self, planet, **kwargs):
-        """
-        Initialize the Rocket object. 
-        Required parameters:
-        :planet:  Reference to a Planet object.
-        
-        Optional keyword parameters are supported:
-        :bitmap:  url of a suitable bitmap image for the rocket (png recommended)
-                    default is `rocket.png`
-        :bitmapscale:  scale factor for bitmap. Default is 0.1
-        :velocity:  initial rocket speed. default is zero.
-        :directiond:  initial rocket direction in degrees. Default is zero.
-        :direction:  initial rocket direction in radians. Default is zero.
-        :tanomalyd:  initial rocket true anomaly in degrees. Default is 90.
-        :tanomaly:  initial rocket true anomaly in radians. Default is pi/2.
-        :altitude:  initial rocket altitude in meters. Default is zero.
-        :showstatus:  boolean displays flight parameters on screen. Default is True.
-        
-        Following parameters may be set as a constant value, or pass in the
-        name of a function that will return the value dynamically or the
-        name of a `ggmath` UI control that will return the value
-        :timezoom:  scale factor for time zoom. Factor = 10**timezoom
-        :heading:  direction to point the rocket in (must be radians)
-        :mass:  mass of the rocket (must be kg)
-        :thrust:  thrust of the rocket (must be N)
-
-        Animation related parameters may be ignored if no sprite animation:
-        :bitmapframe:  ((x1,y1),(x2,y2)) tuple defines a region in the bitmap
-        :bitmapqty:  number of bitmaps -- used for animation effects
-        :bitmapdir:  "horizontal" or "vertical" use with animation effects
-        :bitmapmargin:  pixels between successive animation frames
-        :tickrate:  frequency of spacecraft dynamics calculations (Hz)
-        """
-        self._xy = (1000000,1000000)
-        self.planet = planet
-        self.bmurl = kwargs.get('bitmap', 'rocket.png') # default rocket png
-        self.bitmapframe = kwargs.get('bitmapframe', None) #
-        self.bitmapqty = kwargs.get('bitmapqty', 1) # Number of images in bitmap
-        self.bitmapdir = kwargs.get('bitmapdir', 'horizontal') # animation orientation
-        self.bitmapmargin = kwargs.get('bitmapmargin', 0) # bitmap spacing
-        self.tickrate = kwargs.get('tickrate', 30) # dynamics calcs per sec
-        self.showstatus = kwargs.get('showstatus', True) # show stats
-        self.localheading = 0
-        # dynamic parameters
-        self.timezoom = self.Eval(kwargs.get('timezoom', self.gettimezoom)) # 1,2,3 faster, -1, slower
-        self.heading = self.Eval(kwargs.get('heading', self.getheading)) # must be radians
-        self.mass = self.Eval(kwargs.get('mass', self.getmass)) # kg
-        self.thrust = self.Eval(kwargs.get('thrust', self.getthrust)) # N
-        # end dynamic 
-        super().__init__(self._getposition, 
-            self.bmurl, 
-            self.bitmapframe, 
-            self.bitmapqty, 
-            self.bitmapdir,
-            self.bitmapmargin)
-        self.scale = kwargs.get('bitmapscale', 0.1) # small
-        initvel = kwargs.get('velocity', 0) # initial velocity
-        initdird = kwargs.get('directiond', 0) # initial direction, degrees
-        initdir = kwargs.get('direction', radians(initdird))
-        tanomaly = kwargs.get('tanomaly', pi/2) # position angle
-        tanomaly = radians(kwargs.get('tanomalyd', degrees(tanomaly))) 
-        altitude = kwargs.get('altitude', 0) #
-        r = altitude + self.planet.radius
-        self._xy = (r*cos(tanomaly), r*sin(tanomaly))
-        # default heading control if none provided by user
-        if self.heading == self.getheading:
-            Planet.listenKeyEvent('keydown', 'left arrow', self.turn)
-            Planet.listenKeyEvent('keydown', 'right arrow', self.turn)
-        self.timer = Timer()
-        self.shiptime = 0  # track time on shipboard
-        self.timer.callEvery(1/self.tickrate, self.dynamics)
-        self.lasttime = self.timer.time
-        self.V = [initvel * cos(initdir), initvel * sin(initdir)]
-        self.A = [0,0]
-        # set up status display
-        if self.showstatus:
-            showparms = [self.velocityText,
-                        self.accelerationText,
-                        self.courseDegreesText,
-                        self.altitudeText,
-                        self.thrustText,
-                        self.massText,
-                        self.trueAnomalyDegreesText,
-                        self.scaleText,
-                        self.timeZoomText,
-                        self.shipTimeText]
-            for i in range(len(showparms)):
-                Label((10,10+i*25), showparms[i], size=15, positioning="physical")
-
-    
-    # override or define externally!
-    def getthrust(self):
-        return 0
-
-    # override or define externally!
-    def getmass(self):
-        return 1
-
-    # override or define externally!
-    def getheading(self):
-        return self.localheading
-        
-    # override or define externally!
-    def gettimezoom(self):
-        return 0
-
-    # functions available for reporting flight parameters to UI
-    def velocityText(self):
-        """
-        Report the velocity in m/s
-        """
-        return "Velocity: {0:8.1f} m/s".format(self.velocity)
-        
-    def accelerationText(self):
-        """
-        Report the acceleration in m/s
-        """
-        return "Acceleration: {0:4.1f} m/s²".format(self.acceleration)
-        
-    def courseDegreesText(self):
-        """
-        Report the heading in degrees (zero to the right)
-        """
-        return "Course: {0:6.4f}°".format(degrees(atan2(self.V[1], self.V[0])))
-
-    def thrustText(self):
-        """
-        Report the thrust level in Newtons
-        """
-        return "Thrust: {0:.1f} N".format(self.thrust())
-        
-    def massText(self):
-        """
-        Report the spacecraft mass in kilograms
-        """
-        return "Mass: {0:.1f} kg".format(self.mass())
-        
-    def trueAnomalyDegreesText(self):
-        """
-        Report the true anomaly in degrees
-        """
-        return "True Anomaly: {0:6.4f}°".format(self.tanomalyd)
-        
-    def trueAnomalyRadiansText(self):
-        """
-        Report the true anomaly in radians
-        """
-        return "True Anomaly: {0:6.4f}".format(self.tanomaly)
-        
-    def altitudeText(self):
-        """
-        Report the altitude in meters
-        """
-        return "Altitude: {0:8.1f} m".format(self.altitude)
-        
-    def radiusText(self):
-        """
-        Report the radius (distance to planet center) in meters
-        """
-        return "Radius: {0:8} m".format(self.r)
-        
-    def scaleText(self):
-        """
-        Report the view scale (pixels/meter)
-        """
-        return "View Scale: {0:10.6f} px/m".format(self.planet._scale)
-    
-    def timeZoomText(self):
-        """
-        Report the time acceleration
-        """
-        return "Time Zoom: {0:.1f}".format(float(self.timezoom()))
-        
-    def shipTimeText(self):
-        """
-        Report the elapsed time
-        """
-        return "Elapsed Time: {0:.1f} s".format(float(self.shiptime))
-    
-
-
-            
-    def dynamics(self, timer):
-        # set time duration equal to time since last execution
-        tick = 10**self.timezoom()*(timer.time - self.lasttime)
-        self.shiptime = self.shiptime + tick
-        self.lasttime = timer.time
-        # 4th order runge-kutta method (https://sites.temple.edu/math5061/files/2016/12/final_project.pdf)
-        # and http://spiff.rit.edu/richmond/nbody/OrbitRungeKutta4.pdf  (succinct, but with a typo)
-        self.A = k1v = self.ar(self._xy)
-        k1r = self.V
-        k2v = self.ar(self.vadd(self._xy, self.vmul(tick/2, k1r)))
-        k2r = self.vadd(self.V, self.vmul(tick/2, k1v))
-        k3v = self.ar(self.vadd(self._xy, self.vmul(tick/2, k2r)))
-        k3r = self.vadd(self.V, self.vmul(tick/2, k2v))
-        k4v = self.ar(self.vadd(self._xy, self.vmul(tick, k3r)))
-        k4r = self.vadd(self.V, self.vmul(tick, k3v))
-        self.V = [self.V[i] + tick/6*(k1v[i] + 2*k2v[i] + 2*k3v[i] + k4v[i]) for i in (0,1)]
-        self._xy = [self._xy[i] + tick/6*(k1r[i] + 2*k2r[i] + 2*k3r[i] + k4r[i]) for i in (0,1)]
-        if self.altitude < 0:
-            self.V = [0,0]
-            self.A = [0,0]
-            self.altitude = 0
-
-    # generic force as a function of position
-    def fr(self, pos):
-        self.rotation = self.heading()
-        t = self.thrust()
-        G = 6.674E-11
-        r = Planet.distance((0,0), pos)
-        uvec = (-pos[0]/r, -pos[1]/r)
-        fg = G*self.mass()*self.planet.mass/r**2
-        F = [x*fg for x in uvec]
-        return [F[0] + t*cos(self.rotation), F[1] + t*sin(self.rotation)]
-
-    # geric acceleration as a function of position
-    def ar(self, pos):
-        m = self.mass()
-        F = self.fr(pos)
-        return [F[i]/m for i in (0,1)]
-        
-    def vadd(self, v1, v2):
-        return [v1[i]+v2[i] for i in (0,1)]
-    
-    def vmul(self, s, v):
-        return [s*v[i] for i in (0,1)]
-        
-    def vmag(self, v):
-        return sqrt(v[0]**2 + v[1]**2)
-    
-    def fgrav(self):
-        G = 6.674E-11
-        r = self.r
-        uvec = (-self._xy[0]/r, -self._xy[1]/r)
-        F = G*self.mass()*self.planet.mass/r**2
-        return [x*F for x in uvec]
-    
-    def turn(self, event):
-        increment = pi/50 * (1 if event.key == "left arrow" else -1)
-        self.localheading += increment
-            
-    def _getposition(self):
-        return self._xy
-    
-    @property
-    def xyposition(self):
-        return self._xy
-        
-    @xyposition.setter
-    def xyposition(self, pos):
-        self._xy = pos
-        #self._touchAsset()
-
-    @property
-    def tanomalyd(self):
-        return degrees(self.tanomaly)
-        
-    @tanomalyd.setter
-    def tanomalyd(self, angle):
-        self.tanomaly = radians(angle)
-
-    @property
-    def altitude(self):
-        alt = Planet.distance(self._xy, (0,0)) - self.planet.radius
-        return alt
-        
-    @altitude.setter
-    def altitude(self, alt):
-        r = alt + self.planet.radius
-        self._xy = (r*cos(self.tanomaly), r*sin(self.tanomaly))
-
-    @property
-    def velocity(self):
-        return self.vmag(self.V)
-    
-    @property
-    def acceleration(self):
-        return self.vmag(self.A)
-        
-    @property
-    def tanomaly(self):
-        #pos = self._pos()
-        return atan2(self._xy[1],self._xy[0])
-        
-    @tanomaly.setter
-    def tanomaly(self, angle):
-        r = self.r
-        self._xy = (r*cos(angle), r*sin(angle))
-        self._touchAsset()
-            
-    @property
-    def r(self):
-        return self.altitude + self.planet.radius
-
+        kwargs['thrust'] = self.GetThrust
+        kwargs['mass'] = self.GetMass
+        self.LastTime = 0
+        self.FuelLeft = mdescfuel
+        # Clue for the thrust slider
+        self.lab1 = Label((10,345), "Thrust: up/down key", positioning="physical", size=15)
+        # Define a thrust slider
+        self.ThrustSlider = Slider((10,360), 0, MaxThrottle, 0, positioning="physical", steps=20, leftkey="down arrow", rightkey="up arrow")
+        super().__init__(planet, **kwargs)
+        self.LastTime = self.shiptime
         
         
-    
-
-
-class Planet(MathApp):
-    
-    def __init__(self, **kwargs):
-        """
-        Initialize the Planet object. 
-
-        All of the Rocket optional keyword parameters are supported and will
-        be passed to the rocket class `__init__` function during instantiation.
-        
-        Optional keyword parameters are supported:
-        :viewscale:  pixels per meter in graphics display. Default is 10.
-        :radius:  radius of the planet in meters. Default is Earth radius.
-        :planetmass: mass of the planet in kg. Default is Earth mass.
-        :color: color of the planet. Default is greenish (0x008040).
-        :viewalt: altitude of initial viewpoint in meters. 
-            Default is rocket altitude.
-        :viewanom: true anomaly (angle) of initial viewpoint in radians. 
-            Default is the rocket anomaly.
-        :viewanomd: true anomaly (angle) of initial viewpoing in degrees.
-            Default is the rocket anomaly.
-        """
-        self.scale = kwargs.get('viewscale', 10)  # 10 pixels per meter default
-        self.radius = kwargs.get('radius', 6.371E6) # Earth - meters
-        self.mass = kwargs.get('planetmass', 5.9722E24) # Earth - kg
-        self.color = kwargs.get('color', 0x008040)  # greenish
-        self.kwargs = kwargs # save it for later..
-        super().__init__(self.scale)
-
-    def run(self, rocket):
-        """
-        Execute the Planet (and Rocket) simulation.
-
-        Required parameters:
-        :rocket:  Reference to a Rocket object
-        """
-        self.rocket = rocket
-        self.viewaltitude = self.kwargs.get('viewalt', self.rocket.altitude) # how high to look
-        self.viewanomaly = self.kwargs.get('viewanom', self.rocket.tanomaly)  # where to look
-        self.viewanomalyd = self.kwargs.get('viewanomd', degrees(self.viewanomaly))
-        self.planetcircle = Circle(
-            (0,0), 
-            self.radius, 
-            LineStyle(1, Color(self.color,1)), 
-            Color(self.color,0.5))
-        r = self.radius + self.viewaltitude
-        self.viewPosition = (r*cos(self.viewanomaly), r*sin(self.viewanomaly))
-        super().run()
-
-# test code here
-if __name__ == "__main__":
-    
-    # Lundar Lander Simulation
-    from math import radians, sqrt, log
-    from ggmath import Label, Slider
-    from ggame import Color
-    
-    G = 6.673E-11   # universal gravitation constant
-    moonmass = 7.3477E22
-    moonradius = 1738000
-    alt = 15000     # approximately 15 km orbital altitude
-    vel = sqrt(G*moonmass/(moonradius + alt))
-    
-    # Lunar Lander specifications
-    mascent = 4700          # Ascent stage gross (fueled) mass
-    mdescent = 10344        # Descent stage gross (fueled) mass
-    mdescfuel = 8200        # Descent stage fuel mass
-    Fdmax = 45040           # Descent stage maximum thrust
-    SpecIdesc = 3050        # Descent specific impulse (Ns/kg)
-    mdotmax = Fdmax/SpecIdesc # Maximum propellent flow rate
-    MinThrottle = 0.1       # 10% minimum throttle
-    MaxThrottle = 0.6       # 60% full throttle
-    MinThrust = MinThrottle * Fdmax
-    MaxThrust = MaxThrottle * Fdmax
-    
-    
-    class Lem(Rocket):
-        def __init__(self, planet, **kwargs):
-            kwargs['thrust'] = self.GetThrust
-            kwargs['mass'] = self.GetMass
-            self.LastTime = 0
-            self.FuelLeft = mdescfuel
-            # Clue for the thrust slider
-            self.lab1 = Label((10,345), "Thrust: up/down key", positioning="physical", size=15)
-            # Define a thrust slider
-            self.ThrustSlider = Slider((10,360), 0, MaxThrottle, 0, positioning="physical", steps=20, leftkey="down arrow", rightkey="up arrow")
-            super().__init__(planet, **kwargs)
-            self.LastTime = self.shiptime
-            
-            
-        # Create a function for determining the rocket thrust
-        def GetThrust(self):
-            elapsedtime = self.shiptime - self.LastTime
-            self.LastTime = self.shiptime
-            thrustpct = self.ThrustSlider()
-            if thrustpct < 0.1:
-                return 0
-            elif self.FuelLeft > 0:
-                self.FuelLeft = self.FuelLeft - mdotmax*thrustpct*elapsedtime
-                return Fdmax*thrustpct
+    # Create a function for determining the rocket thrust
+    def GetThrust(self):
+        elapsedtime = self.shiptime - self.LastTime
+        self.LastTime = self.shiptime
+        thrustpct = self.ThrustSlider()
+        if thrustpct < 0.1:
             return 0
-        
-        # Function for calculating the total rocket mass, based on burn time and total
-        # propellent mass.
-        def GetMass(self):
-            return self.FuelLeft + mdescent - mdescfuel + mascent
-        
-        # Function for calculating the percent of fuel remaining, as text
+        elif self.FuelLeft > 0:
+            self.FuelLeft = self.FuelLeft - mdotmax*thrustpct*elapsedtime
+            return Fdmax*thrustpct
+        return 0
     
+    # Function for calculating the total rocket mass, based on burn time and total
+    # propellent mass.
+    def GetMass(self):
+        return self.FuelLeft + mdescent - mdescfuel + mascent
     
-    moon = Planet(planetmass=moonmass, radius=moonradius, color=Color(0x202020,1)) 
-    
-    
-    
-    #Create and "run" the rocket
-    lander = Lem(moon, altitude=alt, velocity=vel)
-    
-    moon.run(lander)
+    # Function for calculating the percent of fuel remaining, as text
+
+
+moon = Planet(planetmass=moonmass, radius=moonradius, viewscale=0.02, color=Color(0x202020,1)) 
+
+
+
+#Create and "run" the rocket
+lander = Lem(moon, altitude=alt, velocity=vel)
+
+moon.run(lander)
