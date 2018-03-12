72dbb067c1e5c436e86fab4fbd0a1bbef655b4f3
Eric Dennison <ericd@netdenizen.com>
2018-03-12 14:22:20 -0400

    Updated from Brython Server: 3/12/2018 2:22:19 PM

--- rocket4bonus.py
+++ rocket4bonus.py
@@ -10,30 +10,53 @@ moonradius = 1738000
 alt = 15000     # approximately 15 km orbital altitude
 vel = sqrt(G*moonmass/(moonradius + alt))
 
+# Lunar Lander specifications
+mascent = 4700          # Ascent stage gross (fueled) mass
+mdescent = 10344        # Descent stage gross (fueled) mass
+mdescfuel = 8200        # Descent stage fuel mass
+Fdmax = 45040           # Descent stage maximum thrust
+SpecIdesc = 3050        # Descent specific impulse (Ns/kg)
+mdotmax = Fdmax/SpecIdesc # Maximum propellent flow rate
+MinThrottle = 0.1       # 10% minimum throttle
+MaxThrottle = 0.6       # 60% full throttle
+MinThrust = MinThrottle * Fdmax
+MaxThrust = MaxThrottle * Fdmax
+
+
 moon = Planet(planetmass=moonmass, radius=moonradius, color=Color(0x202020,1)) 
+
+FuelLeft = mdescfuel
+
+
+# Clue for the thrust slider
+Label((10,345), "Thrust: up/down key", positioning="physical", size=15)
+# Define a thrust slider
+ThrustSlider = Slider((10,360), 0, MaxThrottle, 0, steps=20, leftkey="down arrow", rightkey="up arrow")
+
 
 # Create a function for determining the rocket thrust
 def GetThrust():
+    global FuelLeft
+    global lander
+    global ThrustSlider
+    elapsedtime = lander.shiptime - LastTime
+    LastTime = lander.shiptime
+    thrustpct = ThrustSlider()
+    if thrustpct < 0.1:
+        return 0
+    elif FuelLeft > 0:
+        FuelLeft = FuelLeft - mdotmax*thrustpct*elapsedtime
+        return Fdmax*thrustpct
     return 0
 
-# Function for starting the rocket thrust (called by the START "button")
-def StartRocket():
-    global RocketStarted
-    global StartTime
-    if not RocketStarted:
-        RocketStarted = True
-        # Note the starting time
-        StartTime = rocket.shiptime
-        
 # Function for calculating the total rocket mass, based on burn time and total
 # propellent mass.
 def GetMass():
-    return 1
-
-# Create a button for starting the simulation
-# Physical positioning at 10,400 pixels, calls the StartRocket function
-start = InputButton((10,400), "START", StartRocket, positioning="physical", size=15)
+    return FuelLeft + mdescent - mdescfuel + mascent
 
 #Create and "run" the rocket
 lander = Rocket(moon, thrust=GetThrust, mass=GetMass, altitude=alt, velocity=vel)
+
+LastTime = lander.shiptime
+
 moon.run(lander)
