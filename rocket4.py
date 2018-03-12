02495525dcab25fd4d8e38f1b7991e00e3ce2bc9
Eric Dennison <ericd@netdenizen.com>
2018-03-12 13:01:12 -0400

    Updated from Brython Server: 3/12/2018 1:01:12 PM

--- rocket4.py
+++ rocket4.py
@@ -1,6 +1,6 @@
 from ggrocket import Rocket, Planet
 from math import radians, sqrt, log
-from ggmath import InputButton, Timer, Label
+from ggmath import InputButton, Timer, Label, Slider
 
 earth = Planet(planetmass=0)  # no gravity to simplify things
 
