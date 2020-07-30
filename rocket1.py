07a7a631c6c86c26ffb83ed2660228755d5e9c1e
Eric Dennison <ericd@netdenizen.com>
2020-07-30 17:18:21 -0400

    Updated from Brython Server: 7/30/2020 5:18:20 PM

--- rocket1.py
+++ rocket1.py
@@ -1,5 +1,5 @@
-from ggrocket import Rocket, Planet
+from ggame.astro import Rocket, Planet
 
 planet = Planet(viewscale=0.00005)
-rocket = Rocket(planet, altitude=400000, velocity=1000, timezoom=1)
+rocket = Rocket(planet, altitude=400000, velocity=1000, timezoom=0)
 planet.run(rocket)
